88a8c24e9bf14dfe9a0acfa98119478e30beeb2d
José Nícollas Silva de Souza <josenicollassilvadesouza@gmail.com>
2026-05-20 01:08:55 -0300

    feat: add API geo hollow knight converter

---
+++ apis/geo-hollow-knight/src/controllers/geo.controllers.ts
@@ -0,0 +1,31 @@
+import type { Request, Response } from "express";
+import Geo from "../models/Geo.class.js";
+
+export default class GeoControllers {
+  converterGeo(req: Request, res: Response) {
+    const amount = Number(req.query.amount);
+
+    if (isNaN(amount) || amount <= 0) return res.sendStatus(400);
+
+    const result = Geo.converterGeoToOther(amount);
+    return res.json(result);
+  }
+
+  converterBrl(req: Request, res: Response) {
+    const value = Number(req.query.value);
+
+    if (isNaN(value) || value <= 0) return res.sendStatus(400);
+
+    const result = Geo.converterBrlToGeo(value);
+    return res.json(result);
+  }
+
+  converterUsd(req: Request, res: Response) {
+    const value = Number(req.query.value);
+
+    if (isNaN(value) || value <= 0) return res.sendStatus(400);
+
+    const result = Geo.converterUsdToGeo(value);
+    return res.json(result);
+  }
+}
