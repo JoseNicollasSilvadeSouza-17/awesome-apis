f3efe4fcc8fe356ded9d24c7164ffeab3e4ced8c
José Nícollas Silva de Souza <josenicollassilvadesouza@gmail.com>
2026-06-11 23:21:45 -0300

    feat: add Redis (upstash) in Exoplanet API route Welcome

--- apis/restful/exoplanet/src/utils/constants.ts
+++ apis/restful/exoplanet/src/utils/constants.ts
@@ -1,6 +1,17 @@
-const PORT: number = Number(process.env.PORT) || 3000;
+import z from "zod";
 
-const SUPABASE_URL: string = String(process.env.SUPABASE_URL);
-const SUPABASE_KEY: string = String(process.env.SUPABASE_KEY);
+const PORT: number = z
+	.string()
+	.default("3000")
+	.transform(Number)
+	.parse(process.env.PORT);
 
-export { PORT, SUPABASE_URL, SUPABASE_KEY };
+const SUPABASE_URL: string = z.url().parse(process.env.SUPABASE_URL);
+const SUPABASE_KEY: string = z.string().parse(process.env.SUPABASE_KEY);
+
+const REDIS_URL: string = z.url().parse(process.env.UPSTASH_REDIS_REST_URL);
+const REDIS_TOKEN: string = z
+	.string()
+	.parse(process.env.UPSTASH_REDIS_REST_TOKEN);
+
+export { PORT, SUPABASE_URL, SUPABASE_KEY, REDIS_URL, REDIS_TOKEN };
---
+++ apis/restful/exoplanet/src/utils/redis.ts
@@ -0,0 +1,9 @@
+import { Redis } from "@upstash/redis";
+import { REDIS_TOKEN, REDIS_URL } from "./constants.js";
+
+const redis = new Redis({
+	url: REDIS_URL,
+	token: REDIS_TOKEN
+});
+
+export default redis;
